b7e5e7c6581225cbf0b125acbbde0a933927d089
Kevin P. Dyer <kpdyer@gmail.com>
2015-06-19 10:47:14 -0700

    Issue #18: windows compat.

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
+
 from setuptools import setup
 
+if os.name == 'nt':
+    import py2exe
+
 setup(name='marionette-tg',
+      console=['bin/marionette_client','bin/marionette_server', 'bin/httpserver', 'bin/socksserver'],
       scripts=['bin/marionette_client','bin/marionette_server'],
       test_suite='marionette_tg',
       packages=['marionette_tg','marionette_tg.plugins'],
       package_data={'marionette_tg': ['marionette.conf','formats/*.mar']},
+      zipfile="marionette.zip",
+      options={"py2exe": {
+          "bundle_files": 2,
+          "optimize": 0,
+          "compressed": True,
+          "includes": ['ply','twisted'],
+          "dll_excludes": ["w9xpopen.exe"],
+      }
+      },
       include_package_data=True,
       install_requires=['importlib','twisted','fte','regex2dfa','ply'],
       version='0.0.1-6',
