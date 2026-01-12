142a27e9977c070096f836999af1b03a7cf9de8d
Kevin P. Dyer <kpdyer@dyer.io>
2026-01-12 07:30:25 -0800

    Add dedicated PyPI README
    - Create PYPI_README.md with concise, PyPI-focused content
    - Update setup.py to use PYPI_README.md for long_description
    - Update MANIFEST.in to include PYPI_README.md
    
    The PyPI README is shorter and more focused on:
    - Quick installation
    - Basic usage example
    - Key features
    - Links to full documentation on GitHub

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("PYPI_README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
